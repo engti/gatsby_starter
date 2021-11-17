13d41d2fce5272f255a411442e442cd10d8ff449
Robin Browne <rbrowne@gmail.com>
2021-11-17 12:02:12 +0530

    temp

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations to you
+        Congratulations to you and me
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
