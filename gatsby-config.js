3d8527a981cd7932bde9e300788252a1f3a6dcfb
Robin Browne <rbrowne@gmail.com>
2021-11-18 15:29:52 +0530

    moved gtm into plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,47 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.datasea.in",
     title: "Odds and Ends",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-523CP4T",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: function () {
+          return {
+            pageName: document.title
+          }
+        },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "dataLayer",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "gatsby-route-change",
+        
+        // Defaults to false
+        enableWebVitalsTracking: true,
+        // Defaults to https://www.googletagmanager.com
+        //selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+      },
+    },
+  ],
 };
 
 
-// GTM Plugin Setup
-plugins: [
-  {
-    resolve: "gatsby-plugin-google-tagmanager",
-    options: {
-      id: "GTM-523CP4T",
-
-      // Include GTM in development.
-      //
-      // Defaults to false meaning GTM will only be loaded in production.
-      includeInDevelopment: false,
-
-      // datalayer to be set before GTM is loaded
-      // should be an object or a function that is executed in the browser
-      //
-      // Defaults to null
-      defaultDataLayer: function () {
-        return {
-          pageName: document.title
-        }
-      },
-
-      // Specify optional GTM environment details.
-      // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-      // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-      dataLayerName: "dataLayer",
-
-      // Name of the event that is triggered
-      // on every Gatsby route change.
-      //
-      // Defaults to gatsby-route-change
-      routeChangeEventName: "gatsby-route-change",
-      
-      // Defaults to false
-      enableWebVitalsTracking: true,
-      // Defaults to https://www.googletagmanager.com
-      //selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
-    },
-  },
-]
