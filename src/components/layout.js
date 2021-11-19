cc68a20abe9f3f4dec109b038c4eb586889031db
Robin Browne <rbrowne@gmail.com>
2021-11-19 17:17:44 +0530

    added graphql first query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,34 @@
 import * as React from "react"
-import { container, heading, navLinks, navLinkItem, navLinkText  } from "./layout.module.css"
-import { Link } from "gatsby"
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle  } from "./layout.module.css"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 console.log("container", container)
 
 
 const Layout = ({pageTitle, pageHeading, children}) => {
+    // get site name from graphQl
+    const data = useStaticQuery(graphql`
+    query {
+        site{
+            siteMetadata {
+                title
+            }
+        }
+    }`);
+
+    console.log(data.site.siteMetadata.title);
+
+    // returns the layout
     return(
         <main className = {container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className = {siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
                     <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
                     <li className={navLinkItem}><Link to="/contact" className={navLinkText}>Contact</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <h1 className={heading}>{pageHeading}</h1>
