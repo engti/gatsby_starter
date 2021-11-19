000ebf867680b24d5e1545eca7d2ea9c3060feb0
Robin Browne <rbrowne@gmail.com>
2021-11-19 09:14:09 +0530

    datalayer

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,10 @@ import * as React from "react"
 import Layout from "../components/layout"
 
 const AboutPage = () => {
+    window.dataLayer.push({
+        event: "contact view",
+        pageType: "contact"
+    });
     return(
         <Layout pageHeading="About Page" pageTitle="About Us">
             This what I want people to do on the about page.
